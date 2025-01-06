992290dfd9ddb0f25dec46854b519eb1cb97af16
black32h <blackspetya@gmail.com>
2025-01-06 21:35:06 +0200

    Update NotificationRepository.java

--- src/main/java/com/example/notification1/NotificationRepository.java
+++ src/main/java/com/example/notification1/NotificationRepository.java
@@ -1,4 +1,13 @@
 package com.example.notification1;
 
-public class NotificationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    // Метод для пошуку сповіщень за ID користувача
+    List<Notification> findByUserId(Long userId);
+
+    // Метод для пошуку сповіщень за ID курсу
+    List<Notification> findByCourseId(Long courseId);
 }
